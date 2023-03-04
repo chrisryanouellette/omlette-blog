a41c13a8e35abea1c01dc2f7c96f3b9c67c492ad
Chris <couellette@webstaurantstore.com>
2023-03-04 17:58:22 -0500

    feat: project setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/posts/:slug",
+        destination: "/posts/:slug/1",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
