bcfebfd0237bc6ee14001df67bc19d833a70e00c
Chris <couellette@webstaurantstore.com>
2023-03-06 10:26:43 -0500

    feat: background image

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -13,7 +13,7 @@ export function Layout({ children }: LayoutProps): JSX.Element {
   return (
     <div
       className={concat(
-        "min-h-screen py-4 sm:mx-4 sm:bg-gray-50 sm:drop-shadow-lg md:mx-8 lg:mx-auto lg:max-w-4xl",
+        "relative z-10 min-h-screen bg-gray-50 py-4 sm:mx-4 sm:drop-shadow-lg md:mx-8 lg:mx-auto lg:max-w-4xl",
         robotoMono.className
       )}
     >
